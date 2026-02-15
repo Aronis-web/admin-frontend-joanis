7b8515504ab091a70a01da43eed011e764ad4b63
Aaron Martínez <aaronmarter1@gmail.com>
2026-02-15 02:14:00 -0500

    Fix: Usar resolver personalizado en Metro para forzar axios browser version sin postinstall

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,10 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Disable package exports - we'll use postinstall script to fix axios instead
+// Disable package exports - we'll use custom resolver instead
 config.resolver.unstable_enablePackageExports = false;
 
 // Exclude electron, dist, and web-build folders from Metro bundler
@@ -14,5 +15,24 @@ config.resolver.blockList = [
   /.*\/electron\/.*/,
   /.*\\electron\\.*/
 ];
+
+// Custom resolver to force axios to use browser version
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  // Force axios to use browser version
+  if (moduleName === 'axios') {
+    const axiosBrowserPath = path.join(
+      __dirname,
+      'node_modules/axios/dist/browser/axios.cjs'
+    );
+
+    return {
+      filePath: axiosBrowserPath,
+      type: 'sourceFile',
+    };
+  }
+
+  // Use default resolver for everything else
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
