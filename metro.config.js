bf9dc09409ee56742ac9d3647efaf12de063e79d
Aaron Martínez <aaronmarter1@gmail.com>
2026-02-15 02:07:44 -0500

    Fix: Deshabilitar package exports y usar script fix-axios con postinstall

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Enable package exports to use react-native specific exports (like axios browser version)
-config.resolver.unstable_enablePackageExports = true;
+// Disable package exports - we'll use postinstall script to fix axios instead
+config.resolver.unstable_enablePackageExports = false;
 
 // Exclude electron, dist, and web-build folders from Metro bundler
 config.resolver.blockList = [
