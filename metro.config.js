2b02acb0f5e150cc2b06da645398bcae9d0e4b01
Aaron Martínez <aaronmarter1@gmail.com>
2026-02-15 01:57:50 -0500

    Fix: Habilitar package exports en Metro para usar versión react-native de axios

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Disable package exports to avoid issues with packages that have incorrect exports configuration
-config.resolver.unstable_enablePackageExports = false;
+// Enable package exports to use react-native specific exports (like axios browser version)
+config.resolver.unstable_enablePackageExports = true;
 
 // Exclude electron, dist, and web-build folders from Metro bundler
 config.resolver.blockList = [
